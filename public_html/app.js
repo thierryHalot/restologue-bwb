2de56690e680407c3e4dabfe942221ac9ae012a1
francois <francois@beweb-16.176P>
2018-04-16 16:50:26 +0200

    Add du debut de l'ui

--- public_html/app.js
+++ public_html/app.js
@@ -1,2 +1,22 @@
-alert("ok");
+$(document).ready(function () {
+    var table = $("<table>");
+    $("#listeDeCartes").append(table);
+    $(table).addClass("table table-dark")
+            .append($("<thead>")                            .append($("<th>").text("Prix"))
+                            .append($("<th>").text("Categorie"))
+                    .append($("<tr>")
+                            .append($("<th>").text("Nom"))
+                            .append($("<th>").text("Modifier"))
+                            .append($("<th>").text("Supprimer"))));
+    var tbody = $("<tbody>");
+    $(table).append(tbody);
+    for (var i = 0; i < listeDeFilms.length; i++) {
+        var tr = $("<tr>");
+        $(tr).append($("<td>").text(listeDeFilms[i].titre)).append($("<td>").text(listeDeFilms[i].auteur))
+                .append($("<td>").append($("<td>")).text(listeDeFilms[i].categorie))
+                .append($("<td>").append($("<button>").addClass("btn btn-secondary").text("Modifier")))
+                .append($("<td>").append($("<button>").addClass("btn btn-danger").text("Supprimer")));
+        $(tbody).append(tr);
+    }
+});
 
